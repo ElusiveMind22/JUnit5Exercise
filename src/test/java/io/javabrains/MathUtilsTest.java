f8c193c85f2acc601e2733f3acd9a872813c04ca
Alberto-Ionut Toscariu <62894046+ElusiveMind22@users.noreply.github.com>
2021-03-14 13:13:12 +0200

    Initial commit

---
+++ src/test/java/io/javabrains/MathUtilsTest.java
@@ -0,0 +1,12 @@
+package io.javabrains;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MathUtilsTest {
+    @Test
+    void test()
+    {
+        System.out.println("Am rulat un test!");
+    }
+}
